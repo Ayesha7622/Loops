9e051d187c37743806e80ec336c19063f6110d1d
Ayesha7622 <saddiqa7676@gmail.com>
2025-04-30 17:57:49 +0500

    bdbcbdn

---
+++ android/app/src/main/kotlin/com/example/loops/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.loops
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/Loop_class.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class LoopClass extends StatefulWidget {
+  const LoopClass({super.key});
+
+  @override
+  State<LoopClass> createState() => _LoopClassState();
+}
+
+class _LoopClassState extends State<LoopClass> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+          backgroundColor: const Color.fromARGB(255, 138, 134, 134),
+          title: Text('Whatsapp chats screen Through Loops'),
+        ),
+        body: ListView.builder(
+            itemCount: 10,
+            itemBuilder: (context, i) {
+              return ListTile(
+                leading: CircleAvatar(
+                  child: Text('${i + 1}'),
+                ),
+                title: Text("Item${i + 1}"),
+                subtitle: Text("This is Item number${i + 1}"),
+                trailing: Column(
+                  children: [
+                    Text("Item${i + 1}"),
+                    CircleAvatar(
+                      radius: 5,
+                      backgroundColor: Colors.green,
+                      child: Text("${i + 1}"),
+                    )
+                  ],
+                ),
+              );
+            }));
+  }
+}
