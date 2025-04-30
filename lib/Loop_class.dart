2e04c89955e63dcb49f0c7bb8d32e331ec7a7e5a
Ayesha7622 <saddiqa7676@gmail.com>
2025-04-30 19:32:22 +0500

    hbfcmv

--- lib/Loop_class.dart
+++ lib/Loop_class.dart
@@ -1,5 +1,13 @@
 import 'package:flutter/material.dart';
 
+List listname = [
+  "kse ho",
+  "kb aa rhy ho",
+  "kis din jao gy",
+  "Are u sure",
+  "Ok i'm waiting",
+];
+
 class LoopClass extends StatefulWidget {
   const LoopClass({super.key});
 
@@ -14,7 +22,10 @@ class _LoopClassState extends State<LoopClass> {
         backgroundColor: Colors.black,
         appBar: AppBar(
           backgroundColor: const Color.fromARGB(255, 138, 134, 134),
-          title: Text('Whatsapp chats screen Through Loops'),
+          title: Text(
+            'Whatsapp Chats screen Through Loops',
+            style: TextStyle(fontWeight: FontWeight.w500),
+          ),
         ),
         body: ListView.builder(
             itemCount: 10,
@@ -23,11 +34,22 @@ class _LoopClassState extends State<LoopClass> {
                 leading: CircleAvatar(
                   child: Text('${i + 1}'),
                 ),
-                title: Text("Item${i + 1}"),
-                subtitle: Text("This is Item number${i + 1}"),
+                title: Text(
+                  "Item${i + 1}",
+                  style: TextStyle(color: Colors.white),
+                ),
+                subtitle: Text(
+                  "This is Item number${i + 1}",
+                  style: TextStyle(
+                      color: const Color.fromARGB(255, 190, 186, 186)),
+                ),
                 trailing: Column(
                   children: [
-                    Text("Item${i + 1}"),
+                    Text(
+                      "Item${i + 1}",
+                      style: TextStyle(
+                          color: const Color.fromARGB(255, 179, 178, 178)),
+                    ),
                     CircleAvatar(
                       radius: 8,
                       backgroundColor: Colors.green,
