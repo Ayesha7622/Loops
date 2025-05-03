0e9a77a178034af5947280ac56f16610699a9310
Ayesha7622 <saddiqa7676@gmail.com>
2025-05-03 18:51:11 +0500

    jfvbdf

--- lib/Loop_class.dart
+++ lib/Loop_class.dart
@@ -1,12 +1,70 @@
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
 
-List listname = [
-  "kse ho",
-  "kb aa rhy ho",
-  "kis din jao gy",
-  "Are u sure",
-  "Ok i'm waiting",
-];
+// List listname = [
+//   "kse ho",
+//   "kb aa rhy ho",
+//   "kis din jao gy",
+//   "Are u sure",
+//   "Ok i'm waiting",
+// ];
+
+// class LoopClass extends StatefulWidget {
+//   const LoopClass({super.key});
+
+//   @override
+//   State<LoopClass> createState() => _LoopClassState();
+// }
+
+// class _LoopClassState extends State<LoopClass> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//         backgroundColor: Colors.black,
+//         appBar: AppBar(
+//           backgroundColor: const Color.fromARGB(255, 138, 134, 134),
+//           title: Text(
+//             'Whatsapp Chats screen Through Loops',
+//             style: TextStyle(fontWeight: FontWeight.w500),
+//           ),
+//         ),
+//         body: ListView.builder(
+//             itemCount: 10,
+//             itemBuilder: (context, i) {
+//               return ListTile(
+//                 leading: CircleAvatar(
+//                   child: Text('${i + 1}'),
+//                 ),
+//                 title: Text(
+//                   "Item${i + 1}",
+//                   style: TextStyle(color: Colors.white),
+//                 ),
+//                 subtitle: Text(
+//                   "This is Item number${i + 1}",
+//                   style: TextStyle(
+//                       color: const Color.fromARGB(255, 190, 186, 186)),
+//                 ),
+//                 trailing: Column(
+//                   children: [
+//                     Text(
+//                       "Item${i + 1}",
+//                       style: TextStyle(
+//                           color: const Color.fromARGB(255, 179, 178, 178)),
+//                     ),
+//                     CircleAvatar(
+//                       radius: 8,
+//                       backgroundColor: Colors.green,
+//                       child: Text(
+//                         "${i + 1}",
+//                         style: TextStyle(color: Colors.white, fontSize: 10),
+//                       ),
+//                     )
+//                   ],
+//                 ),
+//               );
+//             }));
+//   }
+// }
+import "package:flutter/material.dart";
 
 class LoopClass extends StatefulWidget {
   const LoopClass({super.key});
@@ -16,51 +74,88 @@ class LoopClass extends StatefulWidget {
 }
 
 class _LoopClassState extends State<LoopClass> {
+  List<String> title = [
+    'Aisha',
+    'Fatima',
+    'Hoorwish',
+    'Manala',
+    'Hadia',
+    'Aina',
+    'Alishba',
+    'Alina',
+    'Ishal',
+    'Reema',
+    'Zahra',
+    'Rumaisa',
+    'Hira',
+    'Zainii',
+    'Hamna',
+    'Bushra',
+    'Amna',
+    'Naina',
+  ];
+  List<String> subTitle = [
+    'Ok ..thk h tm w8 kro',
+    'Yaarrr m sochun g...',
+    'As u Wish',
+    'kl Jao g??',
+    'I know...tm jnti ho',
+    'acha ...kl aun g',
+    'Ooo plzzz yrr..',
+    'kl jana h ya ni',
+    'm kl btaun g',
+    'Vo tmhara poch rhi thi',
+    'sahi h yr',
+    'hn hn relax....No worries',
+    'Routine chng kr k dekho',
+    'hn socho zra is pr',
+    'Okk done',
+    'kl tyar rhnaa...si h na',
+    'm aa rhi hun lny in 2 mins',
+    'Fine broo..',
+  ];
+  List<String> time = [
+    '2:15 am',
+    '5:30 pm',
+    '7:08 pm',
+    '12:00 am',
+    '8:17 am',
+    '4:12 pm',
+    '5:19 am',
+    '9:34 am',
+    '3:55 am',
+    '10:00 pm',
+    '11:23 pm',
+    '7:06 am',
+    '2:38 am',
+    '4:56 am',
+    '8:17 am',
+    '11:23 pm',
+    '5:30 pm',
+    '2:15 am',
+  ];
+  List<String> assets = [
+    'assets/woww.jpg',
+    'assets/rain.jpg',
+    'assets/pgll.jpg',
+    'assets/sunn.JPG',
+    'assets/post.png',
+    'assets/pyarii.png',
+    'assets/nature.jpg',
+    'assets/naaaai.jpg',
+    'assets/nainn.jpg',
+    'assets/izzu.jpg',
+    'assets/intt.jpg',
+    'assets/juliii.jpg',
+    'assets/manuu.jpg',
+    'assets/kipy.jpg',
+    'assets/gaguu.jpg',
+    'assets/gddd.jpg',
+    'assets/fike.jpg',
+    'assets/fatii.jpg',
+  ];
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        backgroundColor: Colors.black,
-        appBar: AppBar(
-          backgroundColor: const Color.fromARGB(255, 138, 134, 134),
-          title: Text(
-            'Whatsapp Chats screen Through Loops',
-            style: TextStyle(fontWeight: FontWeight.w500),
-          ),
-        ),
-        body: ListView.builder(
-            itemCount: 10,
-            itemBuilder: (context, i) {
-              return ListTile(
-                leading: CircleAvatar(
-                  child: Text('${i + 1}'),
-                ),
-                title: Text(
-                  "Item${i + 1}",
-                  style: TextStyle(color: Colors.white),
-                ),
-                subtitle: Text(
-                  "This is Item number${i + 1}",
-                  style: TextStyle(
-                      color: const Color.fromARGB(255, 190, 186, 186)),
-                ),
-                trailing: Column(
-                  children: [
-                    Text(
-                      "Item${i + 1}",
-                      style: TextStyle(
-                          color: const Color.fromARGB(255, 179, 178, 178)),
-                    ),
-                    CircleAvatar(
-                      radius: 8,
-                      backgroundColor: Colors.green,
-                      child: Text(
-                        "${i + 1}",
-                        style: TextStyle(color: Colors.white, fontSize: 10),
-                      ),
-                    )
-                  ],
-                ),
-              );
-            }));
+    return Scaffold();
   }
 }
