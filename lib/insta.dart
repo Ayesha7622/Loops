80462298effa01eb5de6f77b38d9a96d53baecde
Ayesha7622 <saddiqa7676@gmail.com>
2025-05-04 18:31:11 +0500

    jn x x mcn

--- lib/insta.dart
+++ lib/insta.dart
@@ -10,13 +10,24 @@ class InstaClass extends StatefulWidget {
 class _InstaClassState extends State<InstaClass> {
   @override
   List<String> name = [
-    'Alina', 'Ishal', 'Reema', 'Zahra', 'Rumaisa', 'Hira', 'Zainii', 'Hamna',
+    'Your Story', 'Ishal', 'Reema', 'Zahra', 'Rumaisa', 'Hira', 'Zainii',
+    'Hamna',
     'Bushra', 'Amna', 'Naina',
 
     // 'Ishal',
   ];
   List<String> images = [
     'assets/naaaai.jpg',
+    'assets/nainn.jpg',
+    'assets/izzu.jpg',
+    'assets/intt.jpg',
+    'assets/juliii.jpg',
+    'assets/manuu.jpg',
+    'assets/kipy.jpg',
+    'assets/gaguu.jpg',
+    'assets/gddd.jpg',
+    'assets/fike.jpg',
+    'assets/fatii.jpg',
     'assets/nainn.jpg',
     'assets/izzu.jpg',
     'assets/intt.jpg',
@@ -39,32 +50,46 @@ class _InstaClassState extends State<InstaClass> {
               color: Colors.white, fontSize: 30, fontWeight: FontWeight.w600),
         ),
         actions: [
-          Icon(
-            Icons.favorite_border,
-            color: Colors.white,
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Icon(
+              Icons.favorite_border,
+              color: Colors.white,
+            ),
           ),
-          Icon(
-            Icons.chat_bubble_outline_outlined,
-            color: Colors.white,
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Icon(
+              Icons.chat_bubble_outline_outlined,
+              color: Colors.white,
+            ),
           )
         ],
       ),
       body: ListView.builder(
+          scrollDirection: Axis.horizontal,
           itemCount: name.length,
           itemBuilder: (context, i) {
-            return Row(
+            return Column(
               children: [
-                Column(
-                  children: [
-                    CircleAvatar(
-                      radius: 30,
-                      backgroundImage: AssetImage(images[i]),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: CircleAvatar(
+                    radius: 42,
+                    backgroundColor: Colors.pink.shade50,
+                    child: CircleAvatar(
+                      radius: 40,
+                      backgroundColor: Colors.black,
+                      child: CircleAvatar(
+                        radius: 35,
+                        backgroundImage: AssetImage(images[i]),
+                      ),
                     ),
-                    Text(
-                      "${name[i]}",
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  ],
+                  ),
+                ),
+                Text(
+                  "${name[i]}",
+                  style: TextStyle(color: Colors.white),
                 ),
               ],
             );
