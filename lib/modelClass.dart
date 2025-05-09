6fe136af48c13c290f1b10dadedce8a1a8d1a131
Ayesha7622 <saddiqa7676@gmail.com>
2025-05-09 17:57:50 +0500

    hfbvfmnvf

--- lib/modelClass.dart
+++ lib/modelClass.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class ChatsModel {
   String? name;
   String? subName;
-  String? imgaes;
+  String? images;
   String? numbers;
   String? time;
 
@@ -12,7 +12,7 @@ class ChatsModel {
   ChatsModel(
       {required this.name,
       required this.subName,
-      required this.imgaes,
+      required this.images,
       required this.time,
       required this.numbers});
 }
@@ -29,115 +29,115 @@ class _ClassModelState extends State<ClassModel> {
     ChatsModel(
         name: 'Naina',
         subName: 'tm jao to sahi pta chl jye ga',
-        imgaes: 'assets/woww.jpg',
+        images: 'assets/woww.jpg',
         time: '10:12 pm',
         numbers: '1'),
     ChatsModel(
         name: 'Zainab',
         subName: 'ni yr ...mra dil nahi',
-        imgaes: 'assets/yeahh.jpg',
+        images: 'assets/yeahh.jpg',
         time: '1:02 pm',
         numbers: '4'),
     ChatsModel(
         name: 'Rania',
         subName: 'blkl nahii',
-        imgaes: 'assets/yupp.jpg',
+        images: 'assets/yupp.jpg',
         time: '5:33 am',
         numbers: '13'),
     ChatsModel(
         name: 'Asma',
         subName: 'mujhe smjh ni ai',
-        imgaes: 'assets/zavii.jpg',
+        images: 'assets/zavii.jpg',
         time: '9:29 am',
         numbers: '8'),
     ChatsModel(
         name: 'Misbah',
         subName: 'jo khna h kho tm',
-        imgaes: 'assets/sunn.JPG',
+        images: 'assets/sunn.JPG',
         time: '4:05 pm',
         numbers: '5'),
     ChatsModel(
         name: 'Waaniya',
         subName: 'hm idea h mujhe',
-        imgaes: 'assets/tannu.jpg',
+        images: 'assets/tannu.jpg',
         time: '12:00 am',
         numbers: '17'),
     ChatsModel(
         name: 'Eman',
         subName: 'acha si h ...jao pphir',
-        imgaes: 'assets/rain.jpg',
+        images: 'assets/rain.jpg',
         time: '2:12 am',
         numbers: '3'),
     ChatsModel(
         name: 'Zahra',
         subName: 'rest kr lo...thk ho jay g',
-        imgaes: 'assets/wide.jpg',
+        images: 'assets/wide.jpg',
         time: '5:40 pm',
         numbers: '11'),
     ChatsModel(
         name: 'Noor',
         subName: 'hn alhadulillah..',
-        imgaes: 'assets/pyarii.png',
+        images: 'assets/pyarii.png',
         time: '6:09 pm',
         numbers: '12'),
     ChatsModel(
         name: 'Mishhi',
         subName: 'bs tbyt set ni thi',
-        imgaes: 'assets/no.png',
+        images: 'assets/no.png',
         time: '9:55 am',
         numbers: '4'),
     ChatsModel(
         name: 'Fatima',
         subName: 'dimagh na khao mera',
-        imgaes: 'assets/nopee.jpg',
+        images: 'assets/nopee.jpg',
         time: '12:00 pm',
         numbers: '19'),
     ChatsModel(
         name: 'Hareem',
         subName: 'As you wish',
-        imgaes: 'assets/niccc.jpg',
+        images: 'assets/niccc.jpg',
         time: '10:31 pm',
         numbers: '8'),
     ChatsModel(
         name: 'Dua',
         subName: 'hn shyd..kuch time m',
-        imgaes: 'assets/nature.jpg',
+        images: 'assets/nature.jpg',
         time: '7:18 pm',
         numbers: '3'),
     ChatsModel(
         name: 'Hira',
         subName: 'tm poch lo na zra us se',
-        imgaes: 'assets/nainn.jpg',
+        images: 'assets/nainn.jpg',
         time: '4:00 am',
         numbers: '31'),
     ChatsModel(
         name: 'Aqsa',
         subName: 'abhi bt ni kro na',
-        imgaes: 'assets/naaaai.jpg',
+        images: 'assets/naaaai.jpg',
         time: '3:59 pm',
         numbers: '7'),
     ChatsModel(
         name: 'Zunaira',
         subName: 'okk . thk h',
-        imgaes: 'assets/manuu.jpg',
+        images: 'assets/manuu.jpg',
         time: '1:44 am',
         numbers: '3'),
     ChatsModel(
         name: 'KAinat',
         subName: 'mra koi scene ni h',
-        imgaes: 'assets/kipy.jpg',
+        images: 'assets/kipy.jpg',
         time: '6:19 am',
         numbers: '21'),
     ChatsModel(
         name: 'Rabia',
         subName: 'hn thk h ..jse si lgy',
-        imgaes: 'assets/izzu.jpg',
+        images: 'assets/izzu.jpg',
         time: '11:12 am',
         numbers: '6'),
     ChatsModel(
         name: 'Bina',
         subName: 'Khn ja rhi ho??',
-        imgaes: 'assets/juliii.jpg',
+        images: 'assets/juliii.jpg',
         time: '2:00 pm',
         numbers: '5'),
   ];
@@ -168,13 +168,29 @@ class _ClassModelState extends State<ClassModel> {
         body: ListView.builder(
             itemCount: chats.length,
             itemBuilder: (context, index) {
-              return ListTile(
-                leading: CircleAvatar(
-                  backgroundImage: AssetImage(chats[index].imgaes.toString()),
-                ),
-                title: Text(chats[index].name.toString()),
-                subtitle: Text(chats[index].subName.toString()),
-                trailing: Column(
+              return ListTile(onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => ChildClass(
+                        name: chats[index].name.toString(),
+                        subName: chats[index].subName.toString(),
+                        images: chats[index].time.toString(),
+                        time: chats[index].numbers.toString(),
+                        numbers: chats[index].images.toString(),
+                      ),
+                    ));
+              },
+                leading:
+                CircleAvatar(
+                  backgroundImage: AssetImage(chats[index].images.toString()),
+                );
+                title:
+                Text(chats[index].name.toString());
+                subtitle:
+                Text(chats[index].subName.toString());
+                trailing:
+                Column(
                   children: [
                     CircleAvatar(
                       radius: 9,
@@ -186,8 +202,66 @@ class _ClassModelState extends State<ClassModel> {
                     ),
                     Text(chats[index].time.toString()),
                   ],
-                ),
+                );
               );
             }));
+  }
+}
+
+class ChildClass extends StatelessWidget {
+  String name;
+  String subName;
+  String images;
+  String numbers;
+  String time;
+
+  ChildClass({
+    super.key,
+    required this.name,
+    required this.images,
+    required this.subName,
+    required this.numbers,
+    required this.time,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          leading: Icon(Icons.arrow_back),
+          title: Row(children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: CircleAvatar(
+                backgroundImage: AssetImage(images),
+              ),
+            ),
+            Text(
+              name,
+              style: TextStyle(fontSize: 20),
+            ),
+          ]),
+        ),
+        body: Column(
+          children: [
+            SizedBox(
+              height: 30,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                height: 60,
+                width: 200,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.grey),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(subName),
+                ),
+              ),
+            )
+          ],
+        ));
   }
 }
